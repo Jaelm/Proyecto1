bbbf5b3d946a5bf72415e221dfbfc58bde3c835b
PC <PC@DESKTOP-P7H7AB8>
2024-02-04 18:30:50 -0400

    director con actividades creadas

--- src/productores/Director.java
+++ src/productores/Director.java
@@ -6,6 +6,7 @@ package productores;
 
 import classes.EstudioAnimacion;
 import classes.Main;
+import java.util.Random;
 
 /**
  *
@@ -30,6 +31,10 @@ public class Director extends Thread {
         return faltas;
     }
     
+    public void paySueldo(){
+        this.TotalSueldo += (getSueldoHora()*24);
+    }
+    
     public double getSueldoHora(){
         return this.sueldoxHora;
     }
@@ -45,7 +50,68 @@ public class Director extends Thread {
         return saldoFinalManager;
     }
     
-        public int calcSalarioRestado(int faltas){
+    private double calcularGananciasCapTransmitidos(int totalCap, int totalCapPlo, int precioCap, int preciCapPlo) {
+    // Ejemplo de cálculo de ganancias:
+    double ganancias = totalCap * precioCap + totalCapPlo * preciCapPlo;
+    return ganancias;
+    }
+    
+     @Override
+    public void run() {
+        Random random = new Random(); // Generador de números aleatorios
+        while (true) {
+            //System.out.println(Main.gerente.getDaysDeadline());
+            if (estudio.entrega == 0) {
+                int sueldosPagados = calcularSalariosMensuales(isCartoon);
+                int capEntregadosCartoon = Main.CartoonStudio.drive.getTotalCap() + Main.CartoonStudio.drive.getTotalCapPlo();
+                int capEntregadosDisney = Main.DisneyStudio.drive.getTotalCap() + Main.DisneyStudio.drive.getTotalCapPlo();
+                double gananciaBrutaCartoon = calcularGananciasCapTransmitidos(Main.CartoonStudio.drive.getTotalCap(), Main.CartoonStudio.drive.getTotalCapPlo(), 300000, 650000);
+                double gananciaBrutaDisney = calcularGananciasCapTransmitidos(Main.DisneyStudio.drive.getTotalCap(), Main.DisneyStudio.drive.getTotalCapPlo(), 250000, 600000);
+                System.out.println("Capitulos transmitidos de Cartoon NetWork" + capEntregadosCartoon);
+                System.out.println("Ganancia bruta del estudio Cartoon NetWork : " + gananciaBrutaCartoon + "$");
+                System.out.println("carros transmitidos de Disney Channel" + capEntregadosDisney);
+                System.out.println("Ganancia bruta del estudio Disney Channel: " + gananciaBrutaDisney + "$");
+
+                // Enviar capitulos a transmitir
+                System.out.println("Enviando capitulos a transmision...");
+                // Simulamos el tiempo que toma enviar los capitulos
+                try {
+                    Thread.sleep(estudio.dayDurationMs); // 24 horas
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // Realizar tareas administrativas
+                // Verificar lo que está haciendo el manager
+                try {
+                int randomHour = random.nextInt(24); // Generar una hora aleatoria del día
+                int randomMin = random.nextInt(60);
+                for (int i = 0; i < 24; i++) { // Simulando cada hora del día
+                    if (i == randomHour) {
+                        for(int j=0; j<60;j++){
+                            if(j==randomMin){
+                                //cambio a "supervisando"
+                                if(isCartoon){
+                                 
+                                }else{
+                                
+                                }                                
+                                verificarActividadManager();
+                            }
+                            Thread.sleep(estudio.dayDurationMs / (24*60));
+                        }
+                    }
+                    //
+                    Thread.sleep(estudio.dayDurationMs / 24); // Espera una hora en tiempo de simulación
+                }
+                        } catch(InterruptedException e){
+                            e.printStackTrace();
+                        }
+            }
+        }
+    }
+    
+    public int calcSalarioRestado(int faltas){
         int totalRestado = faltas * 100;
         return totalRestado;
     }
@@ -67,5 +133,38 @@ public class Director extends Thread {
             return total;
             }
         }
+    
+    public void verificarActividadManager() throws InterruptedException {
+        System.out.println("Director supervisando al Project Manager");
+
+        if (estudio.manager.estaViendoOnePiece()) {
+            // El gerente está viendo carreras
+            System.out.println("Falta al manager por ver One Piece. Se descontarán $100 de su sueldo.");
+            faltas++;
+            
+            if(isCartoon){
+                
+                int sal = this.calcSalarioRestado(faltas);
+                
+            }else{
+                
+                int sal = this.calcSalarioRestado(faltas);
+                
+            }
+//            
+        } else {
+            // El gerente está trabajando
+            System.out.println("El Project Manager está trabajando.");
+        }
+        Thread.sleep(estudio.dayDurationMs / 57);//sleep 25 minutos
+        //cambio a trabajando
+        if(isCartoon){
+            
+        }else{
+            
+        }
+    }
+
+    
     
 }
