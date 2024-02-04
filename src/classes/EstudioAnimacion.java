db68f6fcc8ee001b82ded40a212efe38d23ca87f
PC <PC@DESKTOP-P7H7AB8>
2024-02-04 15:08:38 -0400

    comienzo clase Estudio

---
+++ src/classes/EstudioAnimacion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+import java.util.concurrent.Semaphore;
+import productores.Worker;
+
+/**
+ *
+ * @author PC
+ */
+public class EstudioAnimacion extends Thread {
+    private String name;
+    public int maxWorkerQty;
+    private Worker[] workers;
+    public int[] emplxDep;
+    public long dayDurationMs;
+    public Drive drive;
+    public Semaphore mutex;
+    public boolean isCartoon;
+    public int entrega;
+    
+    
+    
+}
